612263ed4ad3ba3cb34a6aab9b3f59e83c5a011b
Kevin Mizway <mizzledragon@gmail.com>
2017-02-04 05:31:02 -0500

    Create inital jQuery to create new object from user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
-function chore(name, activity) {
+function task(name, details) {
     this.name = name;
-    this.activity = activity;
+    this.details = details;
 };
+
+$(function() {
+
+    $("#tasks").submit(function(event) {
+        event.preventDefault();
+
+        var name = $("#task-name").val();
+        var details = $("#details").val();
+
+        var newTask = new task(name, details);
+
+        $("#input-task").append("<li>" + newTask.name + "</li>");
+
+    });
+
+
+
+});
